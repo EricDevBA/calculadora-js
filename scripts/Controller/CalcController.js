c33e63f9302f33af7977afb758da6aa5410f7d86
Eric Rosário <ericsantos.r@hotmail.com>
2019-01-31 11:07:01 -0300

    Desenvolvendo da função para exibir números no display

--- scripts/Controller/CalcController.js
+++ scripts/Controller/CalcController.js
@@ -54,7 +54,7 @@
 
      }
 
-     IsOperation(value) {
+     isOperation(value) {
 
          return (['+', '-', '/', '%', '*'].indexOf(value) > -1);
 
@@ -75,14 +75,25 @@
          let last = this._operation.pop();
          let result = eval(this._operation.join(""));
          this._operation = [result, last];
-
-
-
+         this.setLastNumberToDisplay();
 
      }
 
 
      setLastNumberToDisplay() {
+         let lastNumber;
+
+         for (let i = this._operation.length-1; i >= 0; i--) {
+
+             if (!this.isOperation(this._operation[i])) {
+                 lastNumber = this._operation[i];
+                 break;
+
+             }
+
+         }
+         
+         this.displayCalc = lastNumber;
 
 
      }
@@ -92,7 +103,7 @@
 
          if (isNaN(this.getLastOperation())) {
 
-             if (this.IsOperation(value)) { //Trocar o Operador 
+             if (this.isOperation(value)) { //Trocar o Operador 
 
                  this.setLastOperation(value);
 
@@ -102,11 +113,13 @@
 
              } else {
                  this.pushOperation(value);
+                 this.setLastNumberToDisplay();
+
              }
 
          } else {
 
-             if (this.IsOperation(value)) {
+             if (this.isOperation(value)) {
 
                  this.pushOperation(value);
 
